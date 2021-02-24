2cb7b1979be6fd761a5370f9a69ec9af640a5af5
rbeyhum <77794249+rbeyhum@users.noreply.github.com>
2021-02-24 11:10:03 -0500

    Fixed the total price according to number of items and deleted the test.py file

--- shopping_cart.py
+++ shopping_cart.py
@@ -2,32 +2,36 @@
 
 import os 
 import datetime
+import sys
 
 from dotenv import load_dotenv
-
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 products = [
-    {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
-    {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
-    {"id":3, "name": "Robust Golden Unsweetened Oolong Tea", "department": "beverages", "aisle": "tea", "price": 2.49},
-    {"id":4, "name": "Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce", "department": "frozen", "aisle": "frozen meals", "price": 6.99},
-    {"id":5, "name": "Green Chile Anytime Sauce", "department": "pantry", "aisle": "marinades meat preparation", "price": 7.99},
-    {"id":6, "name": "Dry Nose Oil", "department": "personal care", "aisle": "cold flu allergy", "price": 21.99},
-    {"id":7, "name": "Pure Coconut Water With Orange", "department": "beverages", "aisle": "juice nectars", "price": 3.50},
-    {"id":8, "name": "Cut Russet Potatoes Steam N' Mash", "department": "frozen", "aisle": "frozen produce", "price": 4.25},
-    {"id":9, "name": "Light Strawberry Blueberry Yogurt", "department": "dairy eggs", "aisle": "yogurt", "price": 6.50},
-    {"id":10, "name": "Sparkling Orange Juice & Prickly Pear Beverage", "department": "beverages", "aisle": "water seltzer sparkling water", "price": 2.99},
-    {"id":11, "name": "Peach Mango Juice", "department": "beverages", "aisle": "refrigerated", "price": 1.99},
-    {"id":12, "name": "Chocolate Fudge Layer Cake", "department": "frozen", "aisle": "frozen dessert", "price": 18.50},
-    {"id":13, "name": "Saline Nasal Mist", "department": "personal care", "aisle": "cold flu allergy", "price": 16.00},
-    {"id":14, "name": "Fresh Scent Dishwasher Cleaner", "department": "household", "aisle": "dish detergents", "price": 4.99},
-    {"id":15, "name": "Overnight Diapers Size 6", "department": "babies", "aisle": "diapers wipes", "price": 25.50},
-    {"id":16, "name": "Mint Chocolate Flavored Syrup", "department": "snacks", "aisle": "ice cream toppings", "price": 4.50},
-    {"id":17, "name": "Rendered Duck Fat", "department": "meat seafood", "aisle": "poultry counter", "price": 9.99},
-    {"id":18, "name": "Pizza for One Suprema Frozen Pizza", "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
-    {"id":19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend", "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
-    {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
-] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
+    {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50, "price_per": "item"},
+    {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99, "price_per": "item"},
+    {"id":3, "name": "Robust Golden Unsweetened Oolong Tea", "department": "beverages", "aisle": "tea", "price": 2.49, "price_per": "item"},
+    {"id":4, "name": "Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce", "department": "frozen", "aisle": "frozen meals", "price": 6.99, "price_per": "item"},
+    {"id":5, "name": "Green Chile Anytime Sauce", "department": "pantry", "aisle": "marinades meat preparation", "price": 7.99, "price_per": "item"},
+    {"id":6, "name": "Dry Nose Oil", "department": "personal care", "aisle": "cold flu allergy", "price": 21.99, "price_per": "item"},
+    {"id":7, "name": "Pure Coconut Water With Orange", "department": "beverages", "aisle": "juice nectars", "price": 3.50, "price_per": "item"},
+    {"id":8, "name": "Cut Russet Potatoes Steam N' Mash", "department": "frozen", "aisle": "frozen produce", "price": 4.25, "price_per": "item"},
+    {"id":9, "name": "Light Strawberry Blueberry Yogurt", "department": "dairy eggs", "aisle": "yogurt", "price": 6.50, "price_per": "item"},
+    {"id":10, "name": "Sparkling Orange Juice & Prickly Pear Beverage", "department": "beverages", "aisle": "water seltzer sparkling water", "price": 2.99, "price_per": "item"},
+    {"id":11, "name": "Peach Mango Juice", "department": "beverages", "aisle": "refrigerated", "price": 1.99, "price_per": "item"},
+    {"id":12, "name": "Chocolate Fudge Layer Cake", "department": "frozen", "aisle": "frozen dessert", "price": 18.50, "price_per": "item"},
+    {"id":13, "name": "Saline Nasal Mist", "department": "personal care", "aisle": "cold flu allergy", "price": 16.00, "price_per": "item"},
+    {"id":14, "name": "Fresh Scent Dishwasher Cleaner", "department": "household", "aisle": "dish detergents", "price": 4.99, "price_per": "item"},
+    {"id":15, "name": "Overnight Diapers Size 6", "department": "babies", "aisle": "diapers wipes", "price": 25.50, "price_per": "item"},
+    {"id":16, "name": "Mint Chocolate Flavored Syrup", "department": "snacks", "aisle": "ice cream toppings", "price": 4.50, "price_per": "item"},
+    {"id":17, "name": "Rendered Duck Fat", "department": "meat seafood", "aisle": "poultry counter", "price": 9.99, "price_per": "item"},
+    {"id":18, "name": "Pizza for One Suprema Frozen Pizza", "department": "frozen", "aisle": "frozen pizza", "price": 12.50, "price_per": "item"},
+    {"id":19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend", "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99, "price_per": "item"},
+    {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25, "price_per": "item"},
+    {"id":21, "name": "Organic Bananas", "department": "fruits", "aisle": "all fruits", "price": 0.79, "price_per": "pound"}
+] 
+# based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
 
 def to_usd(my_price):
@@ -45,7 +49,7 @@ def to_usd(my_price):
 # defining my grocery store by giving it a name, web address, etc.
 
 grocery_name = "SHOPPERS"
-grocery_web = "https://www.shoopers.com"
+grocery_web = "https://www.shoppers.com"
 grocery_num = "(202) 398-7642"
 grocery_checkout = datetime.datetime.now()
 
@@ -55,19 +59,29 @@ TAX_RATE = os.getenv("TAX_RATE", default=0.0875)
 
 selected_ids=[] #here we create an empty list to add all ids selected
 price_total = 0
+matching_products = []
 
 while True:
-    selected_id = input("Please input a product identfier (1-20 are valid), or 'DONE' if there are no more items: ")
-    if selected_id == "DONE":
+    selected_id = input("Please input a product identfier (1-21 are valid), or 'DONE' if there are no more items: ")
+    if selected_id.upper() == "DONE":
         break
-    elif int(selected_id) > 20 or int(selected_id) < 1:
-        print("Please input a valid identifier. You can resume scanning your items!")
-        exit   
+    elif int(selected_id) > 21 or int(selected_id) < 1:
+        print("Please input a valid identifier. You can resume scanning your items!")   
     else:    
         selected_ids.append(selected_id)
+    for item in products:
+        if str(item["id"]) == str(selected_id):
+            matching_products.append(item)
+            matching_product = matching_products[-1]
+    if matching_product["price_per"] == "item":
+        num_items = int(input("Please enter the number of items desired: "))
+    elif matching_product["price_per"] == "pound":
+        num_pounds = float(input("Please enter the number of pounds desired: "))
+    
 
 # printing the final receipt
-
+receipt_content = ""
+receipt_content += "------------------"+"\n" + grocery_name + "\n" + "------------------" + "\n" + "Web: " + grocery_name + "\n" + "Phone: " + grocery_num + "\n" + "Checkout Time: " + grocery_checkout.strftime("%Y-%m-%d %I:%M:%S %p") + "\n" + "------------------" + "\n" + "SHOPPING CART ITEMS: " + "\n"
 print("------------------")
 print(grocery_name)
 print("------------------")
@@ -77,17 +91,36 @@ print("Checkout Time: ",grocery_checkout.strftime("%Y-%m-%d %I:%M:%S %p"))
 print("------------------")
 print("SHOPPING CART ITEMS: ")
 
+
 for selected_id in selected_ids:
     matching_products = [item for item in products if str(item["id"]) == str(selected_id)]
     matching_product = matching_products[0]
+    if matching_product["price_per"] == "item":
+        price_total = price_total*num_items + matching_product["price"]
+    elif matching_product["price_per"] == "pound":
+        price_total = price_total*num_pounds + matching_product["price"] 
     price_total = price_total + matching_product["price"]
+    receipt_content += "+"+matching_product["name"]+"("+to_usd(matching_product["price"])+")" + "\n" 
     print("+",matching_product["name"],"(",to_usd(matching_product["price"]),")") 
 
-tax = price_total*TAX_RATE
 
+tax = price_total*float(TAX_RATE)
+total_price = price_total + tax
+receipt_content += "------------------" + "\n" + "SUBTOTAL: " + to_usd(price_total) +"\n" + "TAX: " + to_usd(tax) + "\n" + "TOTAL: " + to_usd(total_price) + "\n" + "------------------" + "\n" + "THANK YOU, SEE YOU AGAIN SOON!" + "\n" + "------------------"
 print("------------------")
 print("SUBTOTAL: ",to_usd(price_total))
 print("TAX: ",to_usd(tax))
+print("TOTAL: ",to_usd(total_price))
 print("------------------")
 print("THANK YOU, SEE YOU AGAIN SOON!")
 print("------------------")
+
+
+# ask customer whether customer wants copy of their receipt as a text file 
+copy = input("Would you like to receive a copy of your receipt as a text file [y/n]?")
+if copy == "y":
+    file_name = "my_receipt.txt" # a relative filepath
+    with open(file_name, "w") as file: # "w" means "open the file for writing"
+        file.write(receipt_content)
+elif copy == "n": 
+    exit
